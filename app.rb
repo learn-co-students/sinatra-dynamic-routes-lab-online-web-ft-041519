3969adbfd49b2494e008cda278461f2bb16650e8
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-08 00:16:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,4 +13,30 @@ class App < Sinatra::Base
     "#{@square}"
   end
   
+  # get '/say/:number/:phrase' do
+  #   @num = params[:number].to_i
+  #   @phrase = params[:phrase]
+    
+  #   @num.times do 
+  #     puts "#{@phrase}"
+  #   end
+    
+
+  #   # @num.times {puts "#{@phrase}"}
+  # end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5'
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+  
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+  
+  
+    # "#{params[:word1]}#{params[:word2]}#{params[:word3]}#{params[:word4]}#{params[:word5]} "
+  
+  end
+  
 end
