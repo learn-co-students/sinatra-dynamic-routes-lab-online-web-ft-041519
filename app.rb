3b1a0090477905bacfa15542b15330e6eb85c1d1
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-08 03:19:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,30 +13,34 @@ class App < Sinatra::Base
     "#{@square}"
   end
   
-  # get '/say/:number/:phrase' do
-  #   @num = params[:number].to_i
-  #   @phrase = params[:phrase]
-    
-  #   @num.times do 
-  #     puts "#{@phrase}"
-  #   end
-    
-
-  #   # @num.times {puts "#{@phrase}"}
-  # end
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    # @phrase = params[:phrase]
+    @string = ""
+    @num.times do 
+      @string << params[:phrase]
+    end
+    @string << "."
+  end
   
-  get '/say/:word1/:word2/:word3/:word4/:word5'
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]} "
+  end
   
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
-  
-  
-    # "#{params[:word1]}#{params[:word2]}#{params[:word3]}#{params[:word4]}#{params[:word5]} "
-  
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case params[:operation]
+      when "add"
+        @answer = @num1 + @num2
+      when "subtract"
+        @answer = @num1 - @num2
+      when "multiply"
+        @answer = @num1 * @num2
+      when "divide"
+        @answer = @num1 / @num2
+    end
+    "#{@answer}"
   end
   
 end
